eacb5a6a7569ec97e405764c843f9212642b5cd8
Артём Левко <artyomlevko@gmail.com>
2024-10-04 11:47:44 +0300

    final-final

--- source/protection.cpp
+++ source/protection.cpp
@@ -1,7 +1,11 @@
 #include <time.h>
+#include <string.h>
 #include "protection.h"
 
 static const uint64_t HWORD = (uint64_t)clock();
+
+static uint64_t CANARY_VALUE = (uint64_t)clock();
+static const uint64_t CANARY_HASH  = get_hash(&CANARY_VALUE, sizeof(CANARY_VALUE));
 
 uint64_t get_hash(void* ptr, size_t len)
 {
@@ -14,4 +18,25 @@ uint64_t get_hash(void* ptr, size_t len)
     }
     hash = hash ^ HWORD;
     return hash;
+}
+
+void place_canary (void* dst)
+{
+    for (size_t i = 0; i <= CANARY_THICKNESS - sizeof(CANARY_HASH); i+=sizeof(CANARY_HASH))
+        memcpy((char*)dst + i, &CANARY_HASH, sizeof(CANARY_HASH));
+
+    size_t tail = CANARY_THICKNESS % sizeof(CANARY_HASH);
+    memcpy((char*)dst + CANARY_THICKNESS - tail, &CANARY_HASH, tail);
+}
+
+bool check_canary (void* can)
+{
+    for (size_t i = 0; i <= CANARY_THICKNESS - sizeof(CANARY_HASH); i+=sizeof(CANARY_HASH))
+    {
+        if (memcmp((char*)can + i, &CANARY_HASH, sizeof(CANARY_HASH)) != 0)
+            return false;
+    }
+
+    size_t tail = CANARY_THICKNESS % sizeof(CANARY_HASH);
+    return memcmp((char*)can + CANARY_THICKNESS - tail, &CANARY_HASH, tail) == 0;
 }
--- source/stack.cpp
+++ source/stack.cpp
@@ -13,36 +13,6 @@ static const size_t DUMP_MAX_DATA = 1e3;
 
 #define CHECK_ERR(stk) {if(stack_err(stk)) return;}
 
-#ifdef DO_CANARY
-static const size_t CANARY_THICKNESS = 16; // :8 (optional)
-static uint64_t CANARY_VALUE = (uint64_t)clock();
-static const uint64_t CANARY_HASH  = get_hash(&CANARY_VALUE, sizeof(CANARY_VALUE));
-
-static void place_canary (void* dst);
-static bool check_canary (void* can);
-
-static void place_canary (void* dst)
-{
-    for (size_t i = 0; i <= CANARY_THICKNESS - sizeof(CANARY_HASH); i+=sizeof(CANARY_HASH))
-        memcpy((char*)dst + i, &CANARY_HASH, sizeof(CANARY_HASH));
-
-    size_t tail = CANARY_THICKNESS % sizeof(CANARY_HASH);
-    memcpy((char*)dst + CANARY_THICKNESS - tail, &CANARY_HASH, tail);
-}
-
-static bool check_canary (void* can)
-{
-    for (size_t i = 0; i <= CANARY_THICKNESS - sizeof(CANARY_HASH); i+=sizeof(CANARY_HASH))
-    {
-        if (memcmp((char*)can + i, &CANARY_HASH, sizeof(CANARY_HASH)) != 0)
-            return false;
-    }
-
-    size_t tail = CANARY_THICKNESS % sizeof(CANARY_HASH);
-    return memcmp((char*)can + CANARY_THICKNESS - tail, &CANARY_HASH, tail) == 0;
-}
-#endif // DO_CANARY
-
 #ifdef DO_HASH
 static void save_stack_hash (stack_t* stk);
 static bool check_stack_hash (stack_t* stk);
